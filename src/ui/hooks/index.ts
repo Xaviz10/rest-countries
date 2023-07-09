ae90728456f94afd2806a512863f74754468a29b
Javier Andrés Gamboa Duarte <jgamboa@serempre.com>
2023-07-09 17:01:28 -0500

    release/home: feature all logic ended

--- src/ui/hooks/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./useDateFormatter";
-export * from "./useHookEffect";
-export * from "./useNavigation";
-export * from "./useObjectToArray";
--- src/ui/hooks/useObjectToArray.ts
+++
@@ -1,9 +0,0 @@
-export function useObjectToArray(objectExample: {[key: string]: any}) {
-    const arrayOfKeys = Object.keys(objectExample)
-    const finalArray = arrayOfKeys.map((key,index) => {
-        return(
-        {key: key, value: objectExample[key]} 
-        )
-    })
-    return finalArray
-}
