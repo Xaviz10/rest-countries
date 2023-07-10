36bd32ea3dcae1fc4e00585514b143371bf454e4
Javier Andrés Gamboa Duarte <jgamboa@serempre.com>
2023-07-09 20:40:38 -0500

    Readme file with project description

--- src/ui/validators/contactFormSchema/index.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from "yup";
-import { emailSchema } from "../commonSchemas";
-
-export const contactFormSchema = yup.object({
-  email: emailSchema.required("Required field."),
-  name: yup.string().required("Required field."),
-  message: yup.string().required("Required field."),
-});
